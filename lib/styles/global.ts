730f0aca6668a41e4234050e0273a513c4224b65
David Durlan <david.a.durlan@gmail.com>
2021-01-17 19:37:16 +0100

    🔧 add font & header

--- lib/styles/global.ts
+++ lib/styles/global.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   body {
-    font-family: Roboto;
+    font-family: "Roboto", sans-serif;
     background-color: ${({ theme }) => theme.color.background};
     color: ${({ theme }) => theme.color.primary};
   }
